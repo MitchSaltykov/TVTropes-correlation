7e738f589e5d689c20e66eed2342a138a00bc2a0
Mitch <mitch.saltykov@gmail.com>
2012-05-19 16:31:28 -0400

    initial scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tvtropes::Application.config.secret_token = '51ee9dc90a6351e0c7e4e2d8e12c2894675605e3887d20ca0ba8241775f2f9a251e65bdc31da10584097a6f51c18f7c9a084372cfb9a1baa275a56460b2c9d2d'

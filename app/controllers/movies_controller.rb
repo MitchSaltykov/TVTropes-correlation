06b14afc40be9efe1e60053625939439a66c3fc6
Mitch <mitch.saltykov@gmail.com>
2012-05-20 15:29:56 -0400

    Reject pages without content. Fix stubbing out open-uri calls.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,8 @@ class MoviesController < ApplicationController
 require 'nokogiri'
 require 'open-uri'
 
+EMPTY_PAGE_TEXT = 'Click the edit button to start this new page.'
+
   def index
     @movies = Movie.by_name
   end
@@ -21,12 +23,15 @@ require 'open-uri'
     @movie = Movie.new
     @movie.name = name
     @movie.url = url
+    wikitext = page.search('#wikitext')
+    empty_page = wikitext && wikitext.children.first.text.strip == EMPTY_PAGE_TEXT
+
     respond_to do |format|
-      if @movie.save
+      if !empty_page && @movie.save
         flash[:notice] = 'Movie was successfully created.'
         format.html { redirect_to @movie }
       else
-        flash.now[:error] = 'Movie not created.'
+        flash.now[:error] = "Movie not created. #{'Page has no content' if empty_page}"
         format.html { render :action => :new }
       end
     end
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,22 +2,46 @@ require 'spec_helper'
 
 describe MoviesController do
   describe 'MoviesController#create' do
-    let(:target_url) { 'http://tvtropes.org/pmwiki/pmwiki.php/Film/PitchBlack' }
+    let(:target_url) { 'http://tvtropes.org/movie_url' }
+    #'http://tvtropes.org/pmwiki/pmwiki.php/Film/PitchBlack'
+
     it 'should create a movie from the specified url' do
-      page = <<-PAGE_CONTENTS
+      movie_page = <<-PAGE_CONTENTS
         <div class='pagetitle'>
           Film:  <span>Pitch Black</span>
         </div>
+        <div id='wikitext'>
+          <h2><em>Pitch Black</em> contains examples of:</h2>
+        </div>
+
       PAGE_CONTENTS
 
-      mock_readable = stub(:read => page)
-      MoviesController.stub(:open).and_yield(mock_readable)
+      mock_readable = stub(:read => movie_page)
+      controller.should_receive(:open).and_return(mock_readable)
 
       post :create, :movie => {:url => target_url}
 
       created_movie = Movie.first
       created_movie.name.should == 'Pitch Black'
       created_movie.url.should == target_url
+    end
+
+    it 'should reject movies with no page content' do
+      new_entry_page = <<-PAGE_CONTENTS
+        <div class='pagetitle'>
+          Film:  <span>Pitch Blackasdf</span>
+        </div>
+        <div id='wikitext'>
+          Click the edit button to start this new page.
+        </div>
+      PAGE_CONTENTS
+
+      mock_readable = stub(:read => new_entry_page)
+      controller.should_receive(:open).and_return(mock_readable)
+
+      post :create, :movie => {:url => target_url}
+
+      Movie.count.should == 0
     end
   end
 end
